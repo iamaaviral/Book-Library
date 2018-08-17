931c2c43164ec4c65b3903161a06f7f262db203c
Aviral Srivastava <aviral.srivastava@causecode.com>
2018-08-17 12:29:52 +0530

    adding a basic setup for react native with expo and navigation

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Root, Tabs } from './app/config/router';
+
+ class App extends Component {
+  render() {
+    return <Root />;
+  }
+}
+export default App;
